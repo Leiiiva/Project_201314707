e994cc1e7b1c5a795aa6c141d307ce910cfb03b7
Edgar Leiva <99515843+Leiiiva@users.noreply.github.com>
2022-03-04 22:16:46 -0600

    Corrections - 1st UI
    Corrected UI errors

--- Project_201314707/Project_201314707/Form1.Designer.vb
+++ Project_201314707/Project_201314707/Form1.Designer.vb
@@ -22,10 +22,53 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btn_minimize = New System.Windows.Forms.Button()
+        Me.btn_close = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_minimize
+        '
+        Me.btn_minimize.BackgroundImage = Global.Project_201314707.My.Resources.Resources.minimize
+        Me.btn_minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btn_minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_minimize.ForeColor = System.Drawing.Color.FromArgb(CType(CType(163, Byte), Integer), CType(CType(188, Byte), Integer), CType(CType(182, Byte), Integer))
+        Me.btn_minimize.Location = New System.Drawing.Point(493, 108)
+        Me.btn_minimize.Name = "btn_minimize"
+        Me.btn_minimize.Size = New System.Drawing.Size(30, 30)
+        Me.btn_minimize.TabIndex = 0
+        Me.btn_minimize.UseVisualStyleBackColor = True
+        '
+        'btn_close
+        '
+        Me.btn_close.BackgroundImage = Global.Project_201314707.My.Resources.Resources.close
+        Me.btn_close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_close.ForeColor = System.Drawing.Color.FromArgb(CType(CType(163, Byte), Integer), CType(CType(188, Byte), Integer), CType(CType(182, Byte), Integer))
+        Me.btn_close.Location = New System.Drawing.Point(529, 108)
+        Me.btn_close.Name = "btn_close"
+        Me.btn_close.Size = New System.Drawing.Size(30, 30)
+        Me.btn_close.TabIndex = 1
+        Me.btn_close.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(163, Byte), Integer), CType(CType(188, Byte), Integer), CType(CType(182, Byte), Integer))
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btn_close)
+        Me.Controls.Add(Me.btn_minimize)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents btn_minimize As Button
+    Friend WithEvents btn_close As Button
 End Class
--- Project_201314707/Project_201314707/Form1.vb
+++ Project_201314707/Project_201314707/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub btn_close_Click(sender As Object, e As EventArgs) Handles btn_close.Click
+        Me.Close()
+    End Sub
 
+    Private Sub btn_minimize_Click(sender As Object, e As EventArgs) Handles btn_minimize.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
 End Class
